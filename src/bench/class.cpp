86e8065ef224e473a4210736c0a5b58ad9843d7f
Matthias Treydte <mt@waldheinz.de>
2022-03-13 18:38:38 +0100

    bench/class: benchmark property access

--- src/bench/class.cpp
+++ src/bench/class.cpp
@@ -6,23 +6,35 @@ public:
     rect(double x, double y, double w, double h)
         : x(x), y(y), w(w), h(h) { }
 
+    double get_x() const {
+        return x;
+    }
+
+    void set_x(double x) {
+        this->x = x;
+    }
+
 private:
     double x, y, w, h;
 };
+
+static void register_lua(LuaIntf::LuaContext & ctx) {
+    LuaIntf::LuaBinding(ctx).beginClass<rect>("rect")
+        .addConstructor(LUA_ARGS(double, double, double, double))
+        .addProperty("x", &rect::get_x, &rect::set_x)
+    .endClass();
+}
 
 static void class_ctr(benchmark::State & state) {
     using namespace LuaIntf;
 
     LuaContext ctx { false };
-
-    LuaBinding(ctx).beginClass<rect>("rect")
-        .addConstructor(LUA_ARGS(double, double, double, double))
-    .endClass();
+    register_lua(ctx);
 
     ctx.doString(
-        "fun = function()"
-        "  rect(1, 2, 3, 4)"
-        "end"
+        "fun = function()\n"
+        "  rect(1, 2, 3, 4)\n"
+        "end\n"
     );
 
     auto const fun = ctx.getGlobal("fun");
@@ -33,3 +45,48 @@ static void class_ctr(benchmark::State & state) {
 }
 
 BENCHMARK(class_ctr);
+
+static void class_prop_get(benchmark::State & state) {
+    using namespace LuaIntf;
+
+    LuaContext ctx { false };
+    register_lua(ctx);
+
+    ctx.doString(
+        "local r = rect(1, 2, 3, 4)\n"
+        "fun = function()\n"
+        "  local x = r.x\n"
+        "end\n"
+    );
+
+    auto const fun = ctx.getGlobal("fun");
+
+    for (auto _ : state) {
+        fun();
+    }
+}
+
+BENCHMARK(class_prop_get);
+
+
+static void class_prop_set(benchmark::State & state) {
+    using namespace LuaIntf;
+
+    LuaContext ctx { false };
+    register_lua(ctx);
+
+    ctx.doString(
+        "local r = rect(1, 2, 3, 4)\n"
+        "fun = function()\n"
+        "  r.x = 5\n"
+        "end\n"
+    );
+
+    auto const fun = ctx.getGlobal("fun");
+
+    for (auto _ : state) {
+        fun();
+    }
+}
+
+BENCHMARK(class_prop_set);
