c8cd33e9ed0be5230efe3e18cbc6ac97b736e78e
Matthias Treydte <mt@waldheinz.de>
2022-03-13 11:28:56 +0100

    benc: add map/vector push
    Confirms that 92644d90d5b9160d6ae31448f351ebcbe921cf70
    actually helps.

--- src/bench/main.cpp
+++ src/bench/main.cpp
@@ -1,7 +1,13 @@
 #include <benchmark/benchmark.h>
 #include <LuaIntf.h>
 
-#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+namespace LuaIntf {
+    LUA_USING_LIST_TYPE(std::vector)
+    LUA_USING_MAP_TYPE(std::unordered_map)
+}
 
 static void ctx_instantiate(benchmark::State & state) {
     for (auto _ : state) {
@@ -51,5 +57,33 @@ static void global_call(benchmark::State & state) {
 }
 
 BENCHMARK(global_call);
+
+static void global_set_map_int(benchmark::State & state) {
+    LuaIntf::LuaContext ctx { false };
+
+    std::unordered_map<int, int> m;
+
+    for (int k = 0; k < state.range(0); k++) {
+        m.insert({ k, 42 });
+    }
+
+    for (auto _ : state) {
+        ctx.setGlobal("foo", m);
+    }
+}
+
+BENCHMARK(global_set_map_int)->Range(1, 1024);
+
+static void global_set_vector_int(benchmark::State & state) {
+    LuaIntf::LuaContext ctx { false };
+
+    std::vector<int> m(state.range(0), 42);
+
+    for (auto _ : state) {
+        ctx.setGlobal("foo", m);
+    }
+}
+
+BENCHMARK(global_set_vector_int)->Range(1, 16 * 1024);
 
 BENCHMARK_MAIN();
